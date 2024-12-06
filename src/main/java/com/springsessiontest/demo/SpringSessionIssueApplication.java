218d49b52059e14582df2af6e8c60c201da90bfe
chittim <lokanadham412@gmail.com>
2024-12-06 23:47:03 +0530

    initial commit for spring session issue

---
+++ src/main/java/com/springsessiontest/demo/SpringSessionIssueApplication.java
@@ -0,0 +1,13 @@
+package com.springsessiontest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionIssueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSessionIssueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springsessiontest/demo/SpringSessionIssueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springsessiontest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionIssueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
